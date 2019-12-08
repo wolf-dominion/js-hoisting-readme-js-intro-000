9c365b269cd6bc4282b87015090566f836540dbf
Hannah Drury <hannah_drury@yahoo.com>
2019-12-08 04:17:34 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,7 +3,7 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
+  lyric = "maybe";
   return lyric;
 }
 
